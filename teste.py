14dd1ec4061a8534635c0548f762bc1c5df6e646
Eduardo Andrade <eduric2001@gmail.com>
2022-01-25 15:34:09 -0300

    Correções de Arquivos

--- teste.py
+++
@@ -1,9 +0,0 @@
-frase_UM = 'ola'
-print(frase_UM)
-frase_DOIS = 'eduardo'
-print(frase_DOIS)
-for c in range(1, 3):
-    frase_soma = frase_UM + frase_DOIS
-    print(frase_soma)
-    frase_UM = frase_soma + frase_UM
-    print(frase_UM)
